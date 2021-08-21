19c4ce85e2effef900f61046ad245541cca1ce70
Kien Luu <luutrungkien120894@gmail.com>
2021-08-21 17:04:41 +0700

    update anthoer words

--- controller.js
+++ controller.js
@@ -3,18 +3,39 @@ const defaultQuote = '"“People where you live," the little prince said, "grow 
 const defaultAuthor = "- Little Prince （Antoine de Saint-Exupéry) -"
 const googleAPI = "https://www.google.com/search?q="
 const wikiAPI = "https://en.wikipedia.org/w/api.php"
+const randomWordAPI = "https://random-words-api.vercel.app/word"
+const dictionaryAPI = "https://api.dictionaryapi.dev/api/v2/entries/en/{0}"
+
 var quote = $('#quote')
 var authorDiv = $('#author .author-text')
 var anotherQuoteBtn = $('.another-quote a')
+var anotherWordsBtn = $('.another-word a')
 var authorImg = $('#author-img img')
 var authorInfo = $('#author-img .info .des')
 var currentAuthor
 var text_story = $('.text_story')
 
+function getWord(callback) {
+	$.ajax({
+		type: "GET",
+		url: randomWordAPI,
+			dataType: 'json',
+		success: callback
+	});
+}
 
+function getDefination(word, callback, callbackFail) {
+	$.ajax({
+		type: "GET",
+		url: dictionaryAPI.replace('{0}', word),
+			dataType: 'json',
+		success: callback,
+		error: callbackFail
+	});
+}
 
 authorDiv.on('click', function (event) {
-	url = googleAPI + currentAuthor;
+	url = googleAPI + currentSearch;
 	openWindown(url)
 })
 
@@ -23,7 +44,7 @@ function getImg(titles, callback) {
 		action: 'query',
 		titles: titles,
 		prop: 'pageimages|extracts',
-    explaintext: 1,
+			explaintext: 1,
 		format: 'json',
 		pithumbsize: '720'
 	}
@@ -35,8 +56,8 @@ function getImg(titles, callback) {
 			},
 		url: wikiAPI,
 		data: params,
-    crossDomain: true,
-    dataType: 'jsonp',
+			crossDomain: true,
+			dataType: 'jsonp',
 		success: callback
 	});
 }
@@ -77,94 +98,166 @@ function typeEffect(string, classAffect, timeout = 0) {
 function changeLogoImg(fileName) {
 	$('#logo .logo img')[0].src = fileName
 }
-$('.bulk-toggle').on('click', function (e) {
-	value = $(e.currentTarget).is(":checked")
-	if (value) {
+
+
+function setTheme(val) {
+	if (val) {
 		$('body').addClass('dark-theme')
 		changeLogoImg('logo-dark-mode.svg')
+		setCookie('light-theme', 'on', 30)
 	} else {
 		$('body').removeClass('dark-theme')
 		changeLogoImg('logo.svg')
+		setCookie('light-theme', 'off', 30)
 	}
+}
+$('.bulk-toggle').on('click', function (e) {
+	value = $(e.currentTarget).is(":checked")
+	setCookie('light-theme', 'on', 30)
+	setTheme(value)
 })
 
 function getAuthorImg(author) {
-  getImg(author, function (data) {
-    setAuthorImg(data)
-    $('#author')[0].style.pointerEvents = ''
-  })
+	getImg(author, function (data) {
+		setAuthorImg(data)
+		$('#author')[0].style.pointerEvents = ''
+	})
 }
 
 function setAuthorImg(data) {
-  try {
-    if (data.query.pages[Object.keys(data.query.pages)[0]]) {
-      authorImg[0].src = data.query.pages[Object.keys(data.query.pages)[0]].thumbnail.source
-      authorInfo[0].innerHTML ='<p>' + data.query.pages[Object.keys(data.query.pages)[0]].extract + '</p>'
-    }
-  } catch (err) {
-    $('#author-img')[0].style.display = 'none'
-  }
-  
+	try {
+		if (data.query.pages[Object.keys(data.query.pages)[0]]) {
+			authorImg[0].src = data.query.pages[Object.keys(data.query.pages)[0]].thumbnail.source
+			authorInfo[0].innerHTML ='<p>' + data.query.pages[Object.keys(data.query.pages)[0]].extract + '</p>'
+		}
+	} catch (err) {
+		$('#author-img')[0].style.display = 'none'
+	}
+
 }
 
 
 
 function callbackAfterGetQuote(data) {
 	if (data.content) {
-		setInfo(data.content)
-		setAuthor(data.author)
+		setQuote(data.content, data.author, 1000)
 	} else {
-		setQuote(defaultQuote)
-		setAuthor(defaultAuthor)
+		setQuote(defaultQuote,defaultAuthor, 1000)
 	}
-  $('#author')[0].style.pointerEvents = 'none'
-  getAuthorImg(data.author)
+		$('#author')[0].style.pointerEvents = 'none'
+		getAuthorImg(data.author)
 
 	text_story[0].style.display = 'block'
+	setLoading(false)
 }
 
 
 anotherQuoteBtn.on('click', function (e) {
+	setLoading(true)
 	getQuote(function (data) {
-    $('#author')[0].style.pointerEvents = 'none'
-    getAuthorImg(data.author)
+		$('#author')[0].style.pointerEvents = 'none'
+		$('#author-img')[0].style.display = ''
+		getAuthorImg(data.author)
 		$('.css-typing')[0].style.opacity = 0;
 		if (data.content) {
 			setQuote(data.content, data.author, 1000)
 		} else {
 			setQuote(defaultQuote, defaultAuthor, 1000)
 		}
+
+		setLoading(false)
 		})
+})
+
+
+function callDefinationAPI(data) {
+	getDefination(data[0].word, function(result) {
+		setWordAndDefination(result[0].word, result[0].meanings[0].definitions[0].definition)
+		setLoading(false)
+	}, function (err) {
+		if (err.status == '404') {
+			getWord(callDefinationAPI)
+		}
+	})
+}
+
+
+function setWordAndDefination(word, definition) {
+	$('.css-typing')[0].style.opacity = 0;
+	$('#author-img')[0].style.display = 'none'
+	typeEffect(word, '#word.css-typing', 1000)
+	typeEffect(definition, '#quote.css-typing', 1000)
+	typeEffect('Click To See More','#author .author-text', 1000)
+	currentSearch = word
+}
+
+anotherWordsBtn.on('click', function (e) {
+	setLoading(true)
+	getWord(callDefinationAPI)
 })
 
 
 function setQuote(content, author, timeout = 0) {
 	let authorText = '- ' + author + ' -'
-	currentAuthor = author
-	typeEffect(content, '.css-typing', timeout)
+	currentSearch = author
+	typeEffect(content, '#quote.css-typing', timeout)
 	typeEffect(authorText,'#author .author-text', timeout)
 }
 
-function setInfo(content) {
-	typeEffect(content, '.css-typing')
+
+
+function setLoading(val) {
+	if (val) {
+		$('.wrapper-loading')[0].style.display = 'block'
+		$('.lds-ellipsis')[0].style.display = 'block'
+	} else {
+		$('.wrapper-loading')[0].style.display = 'none'
+		$('.lds-ellipsis')[0].style.display = 'none'
+	}
 }
-
-
-function setAuthor(author) {
-	let authorText = '- ' + author + ' -'
-	currentAuthor = author
-		typeEffect(authorText,'#author .author-text')
-
-}
-
-
-
 function openWindown(url) {
 	window.open(url,"_blank")
 }
 
+function checkCookie() {
+  let lightTheme = getCookie("light-theme");
+  if (lightTheme == 'on') {
+		setTheme(true)
+		$('.bulk-toggle').attr("checked", true);
+  } else {
+		setTheme(false)
+		$('.bulk-toggle').attr("checked", false);
+  }
+}
+
+
+function setCookie(cname,cvalue,exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 function init() {
+	setLoading(true)
 	getQuote(callbackAfterGetQuote);
+	checkCookie()
 }
 
 init()
---
+++ firebase-controller.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBeAFcMXi1MoCGT5lVgBZtp5r66uH1rK2k",
+    authDomain: "abditory-1629394191594.firebaseapp.com",
+    projectId: "abditory-1629394191594",
+    storageBucket: "abditory-1629394191594.appspot.com",
+    messagingSenderId: "10699157102",
+    appId: "1:10699157102:web:1eafc6ef771fdaf162007f",
+    measurementId: "G-ZXF8VVCK0E"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
